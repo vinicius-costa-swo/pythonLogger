9195e3fbe7753fdf08674f4d5121fb96012867d4
vinicius-costa-swo <115734967+vinicius-costa-swo@users.noreply.github.com>
2023-02-21 01:19:38 -0300

    Added Logger Module

---
+++ logger.py
@@ -0,0 +1,34 @@
+#Importing Libraries
+import logging
+import socket
+
+#Fetching Hostname
+logger = logging.getLogger(socket.gethostname())
+
+class logger_config:
+    def debug(debug):
+        logger = logging.getLogger(socket.gethostname())
+        formatter = logging.Formatter('%(asctime)s : %(name)s : %(levelname)s : %(message)s')
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        if not logger.handlers:
+            logger.addHandler(handler)
+        if debug:
+            logger.setLevel(logging.DEBUG)
+        if not debug:
+            logger.setLevel(logging.INFO)
+        logger.propagate = False      
+    
+
+class log:
+    def INFO(x):
+        logger.info(x)
+    def DEBUG(x):
+        logger.debug(x)
+    def WARNING(x):
+        logger.warning(x)
+    def CRITICAL(x):
+        logger.critical(x)     
+
+       
+
