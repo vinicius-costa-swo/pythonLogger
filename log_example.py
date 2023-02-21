d01585488a87a3dfd298ac8aeed95139efa61df2
vinicius-costa-swo <115734967+vinicius-costa-swo@users.noreply.github.com>
2023-02-21 01:20:07 -0300

    Added Log Example File

---
+++ log_example.py
@@ -0,0 +1,8 @@
+from log import logger_config, log
+
+logger_config.debug(True)
+
+log.INFO('info message')
+log.DEBUG('debug message')
+log.WARNING('warning message')
+log.CRITICAL('critical message')
